f8d2742eb4304be787af3cbe7621d4579baf29db
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-11-02 10:08:33 -0400

    saving 3Sum solution

--- Typescript/ThreeSum/index.ts
+++ Typescript/ThreeSum/index.ts
@@ -1,70 +1,41 @@
 /*
 
-class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> res = new ArrayList<>();
-    
-    if (nums.length == 3) {
-        if (nums[0] + nums[1] + nums[2] == 0) {
-            List<Integer> triplet = new ArrayList<>();
-            triplet.add(nums[0]);
-            triplet.add(nums[1]);
-            triplet.add(nums[2]);
-            res.add(triplet);
-        }
-        return res;
-    }
-    
-    return res;
-}
-}
 
-
-Sort the given array in non-decreasing order.
-Loop through the array from index 0 to n-1.
-For each iteration, set the target as -nums[i].
-Set two pointers, j=i+1 and k=n-1.
-While j<k, check if nums[j]+nums[k]==target.
-If yes, add the triplet {nums[i], nums[j], nums[k]} to the result and move j to the right and k to the left.
-If no, move either j or k based on the comparison of nums[j]+nums[k] with target.
-To avoid duplicate triplets, skip the iterations where nums[i]==nums[i-1] and also skip the iterations where nums[j]==nums[j-1] or nums[k]==nums[k+1].
 */
 
 function threeSum(nums: number[]): number[][] {    
-    nums.sort((a, b) => a - b);   
-    const dblArray : number[][] = [];
+    nums.sort((a, b) => a - b);   //sort non decreasing
+    const res : number[][] = [];
     const n = nums.length;
     for (let i = 0; i < n-1;i++){
-        let target = -nums[i];
+        if (i > 0 && nums[i] === nums[i-1]) continue;
         let j = i + 1;
         let k = n -1;
         while (j < k){
-            if (nums[i]===nums[i-1]) break;
-            if (nums[j]===nums[j-1]) break;
-            if (nums[k]===nums[k+1]) break;
-         
-            if (nums[j] + nums[k] == target){
-                let triplet = [nums[i], nums[j], nums[k]];
-                dblArray.push(triplet);
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0){
+                res.push([nums[i], nums[j], nums[k]]);
+                while (j < k && nums[j] === nums[j +1]) j++;
+                while (j < k && nums[k] === nums[k -1]) k--;            
                 j++;
-                k--; //verify
-            } else{
-                   if (nums[j] + nums[k] < target){
-                      j++;
-                  } else {
+                k--;
+            } else if (sum < 0){
+                    j++;
+                   } else {
                     k--;
-                  }
+                   }
             
-        }
-    }
+        }    
 }
-    return dblArray;
+    return res;
 };
 
 
 function main(){
 const nums = [-1,0,1,2,-1,-4];
 let result = threeSum(nums);
+
+console.dir("Result: " + result)
 
 
 
