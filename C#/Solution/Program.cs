e5a9e994e8e5a0247c95117df5ec3cbb346dbefc
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-11-20 10:47:25 -0500

    practicing C#

---
+++ C#/Solution/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            int[] input = { 0, 1, 2, 2, 3, 0, 4, 2 };
+
+            RemoveElement(input, 2);
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int counter = 0;           
+          
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!Equals(nums[i], val))
+                {
+                    nums[counter] = nums[i];
+                    counter++;
+                } 
+            }  
+            return counter;
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            Console.WriteLine(string.Join(", ", nums));
+            int[] unique = nums.Distinct().ToArray();
+            Console.WriteLine(string.Join(", ", unique));
+            int k = unique.Length;
+
+            for (int i = 0; i < k; i++)
+            {
+                System.Console.WriteLine("Placing " + unique[i] + " at index " + i);
+                nums[i] = unique[i];
+            }
+            return k;
+        }
+
+    }
+}
