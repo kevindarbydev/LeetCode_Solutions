825b5076bf43068f69771321527601e1c19ded34
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2024-05-03 12:25:42 -0400

    attempt at todays daily, 74/84 test cases passed

---
+++ Java/CompareVersionNumbers/Solution.java
@@ -0,0 +1,38 @@
+package Java.CompareVersionNumbers;
+
+public class Solution {
+    public static int compareVersion(String version1, String version2) {
+        StringBuilder version1sb = new StringBuilder();
+        StringBuilder version2sb = new StringBuilder();
+        String[] version1Parts = version1.split("\\.");
+        String[] version2Parts = version2.split("\\.");
+
+        int v1Counter = 0;
+        int v2Counter = 0;
+
+        int maxParts = Math.max(version1Parts.length, version2Parts.length);
+        for (int i = 0; i < maxParts; i++) {
+            int current1 = (i < version1Parts.length) ? Integer.parseInt(version1Parts[i]) : 0; // check that we're not out of bounds
+            int current2 = (i < version2Parts.length) ? Integer.parseInt(version2Parts[i]) : 0; // on either part array, if we are, default to 0
+
+            if (current1 > current2) {
+                return 1;
+            } else if (current2 > current1) {
+                return -1;
+            }
+        }
+
+        // If we reach here, all parts are equal, compare the lengths
+        if (version1Parts.length > version2Parts.length) {
+            return 1; // version1 is greater
+        } else if (version2Parts.length > version1Parts.length) {
+            return -1; // version2 is greater
+        } else {
+            return 0; // versions are equal
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compareVersion("7.5.2.4","7.5.3"));
+    }
+}
