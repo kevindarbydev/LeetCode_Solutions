60b9990a5f5c0f86c9c5db4b2b80d992b71bf4e5
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2025-11-10 14:58:35 -0500

    reverse integer ts

---
+++ Typescript/reverseInteger/index.ts
@@ -0,0 +1,33 @@
+function reverse(x: number): number {
+  const MIN = -2147483648;
+  const MAX = 2147483648;
+  var response = x.toString();
+  let stringBuilder = "";
+
+  for (let i = response.length - 1; i >= 0; i--) {
+    stringBuilder = stringBuilder.concat(response.charAt(i));
+  }
+
+  if (x < 0) {
+    // remove the '-' that got moved to the end
+    stringBuilder = stringBuilder.replace("-", "");
+    // convert to number and reapply sign
+    if (-Number.parseInt(stringBuilder) < MIN) {
+      return 0;
+    } else return -Number.parseInt(stringBuilder);
+  } else {
+    if (Number.parseInt(stringBuilder) > MAX) {
+      return 0;
+    } else return Number.parseInt(stringBuilder);
+  }
+}
+
+function main() {
+  var numOne = 131;
+  var numTwo = -123;
+  console.log(reverse(numTwo));
+  console.log("-----");
+  //console.log(reverse(numTwo))
+}
+
+main();
